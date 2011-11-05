7c1af951692f8b2bf4d36469ce249c103b41ec81
Valder Gallo <valdergallo@gmail.com>
2011-11-05 17:43:13 -0200

    [add] get_list_dir

--- fabfile.py
+++ fabfile.py
@@ -22,6 +22,7 @@ def _save_config(project_path):
     "Private create config.yaml file"
 
     yc.set(host=env.host)
+    yc.set(project=env.project)
     yc.set(path=project_path)
     yc.set(python_path=env.python_path)
     yc.save()
@@ -43,7 +44,35 @@ def requeriments(textfile):
     fileopen.close()
 
 
-def build(project_path=env.path):
+def get_list_dir():
+    dirs = []
+    dirList = os.listdir(env.python_path)
+    for dir in dirList:
+        dirs.append(os.path.join(env.python_path, dir))
+    return dirs
+
+
+def create_htaccess():
+    "Create .htaccess"
+    dirList = get_list_dir()
+
+    htaccess = """
+SetHandler python-program
+PythonHandler django.core.handlers.modpython
+SetEnv DJANGO_SETTINGS_MODULE {0}.src.settings
+PythonInterpreter {0}
+PythonOption django.root /{0}
+PythonPath "{1} + sys.path"
+""".format(env.project, dirList)
+
+    file_open = open('.htaccess','w')
+    file_open.write(htaccess)
+    file_open.close()
+
+    print "Created .htaccess file"
+
+
+def build(project='django', project_path=env.path):
     "Build packages in server"
 
     _save_config(project_path)
