88ac387ebeb22edc3228fde201d2a42527a66c6f
Valder Gallo <valdergallo@gmail.com>
2011-11-05 21:08:30 -0200

    [nd] temp config

--- settings.py
+++ settings.py
@@ -9,4 +9,4 @@ env.project = 'teste'
 env.hosts = ['valdergallo.com.br']
 env.user = 'valdergall'
 env.path = '~/public_html/teste/'
-env.python_path = os.path.join(BASEDIR,'eggs')
+env.python_path = '~/eggs/'
