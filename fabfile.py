69247d195e016b658b9e6739091ced5de6cb2996
Valder Gallo <valdergallo@gmail.com>
2011-11-06 16:48:27 -0200

    [add] Build options finished

--- fabfile.py
+++ fabfile.py
@@ -5,11 +5,14 @@ import os
 from fabric.api import *
 from fabric.colors import red, green, yellow
 from fabric.contrib.console import confirm
+from fabric.contrib.project import upload_project, rsync_project
+from fabric.operations import put
 from settings import *
 from fabric.state import output
 
 output['status'] = False
 output['running'] = False
+
 
 def test():
     "Test connection with server"
@@ -18,10 +21,12 @@ def test():
     run('uname -a')
     run('python -V')
 
+
 def pip(egg):
     "Install eggs"
     output = run("mkdir -p %s" % env.python_path, pty=False)
-    output = run('easy_install -Zmaxd %s %s' % (env.python_path, egg), pty=False)
+    output = run('easy_install -Zmaxd %s %s' %
+            (env.python_path, egg), pty=False)
 
 
 def requeriments(textfile):
@@ -39,7 +44,7 @@ def _get_list_dir():
     dirs = []
     dirList = run('ls %s' % env.python_path)
     for dir in dirList.split('  '):
-        dirs.append(env.python_path+dir)
+        dirs.append(env.python_path + dir)
     dirs.append(env.path)
     return dirs
 
@@ -48,8 +53,7 @@ def create_htaccess():
     "Create .htaccess"
     dirList = _get_list_dir()
 
-    htaccess = """
-SetHandler python-program
+    htaccess = """SetHandler python-program
 PythonHandler django.core.handlers.modpython
 SetEnv DJANGO_SETTINGS_MODULE {0}.src.settings
 PythonInterpreter {0}
@@ -57,11 +61,21 @@ PythonOption django.root /{0}
 PythonPath "{1} + sys.path"
 """.format(env.project, dirList)
 
-    file_open = open('.htaccess','w')
+    name = '.htaccess'
+    file_open = open(name, 'w')
     file_open.write(htaccess)
     file_open.close()
-
+    run('mkdir -p %s' % env.path)
+    put(os.path.realpath(name), env.path)
     print "Created .htaccess file"
+
+
+def upload():
+    project = os.path.dirname(__file__)
+    remote = env.path
+    run('mkdir -p %s' % remote)
+
+    upload_project(project, remote)
 
 
 def build():
@@ -73,4 +87,6 @@ def build():
     print(yellow('Create config'))
     create_htaccess()
 
+    print(yellow('Send files'))
+    upload()
 
